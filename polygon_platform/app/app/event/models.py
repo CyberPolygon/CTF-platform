74e1bed436c49b5bb335a7221514d4c60c1514db
ar.chusovitin <ar.chusovitin@tensor.ru>
2021-01-11 16:02:45 +0700

    https://trello.com/c/K4Ziu53s

--- polygon_platform/app/app/event/models.py
+++ polygon_platform/app/app/event/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from polygon_platform.app.app.tasks.models import Task
 
 
 class Event(models.Model):
     """
     Модель списка событий
     """
-    pass
+    name = models.CharField(max_length=256)
+    picture = models.ImageField(upload_to='uploads/picture/')
+    description = models.TextField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    datebegin = models.DateTimeField()
+    dateend = models.DateTimeField()
