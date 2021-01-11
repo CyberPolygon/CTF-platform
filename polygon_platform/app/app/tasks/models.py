27ba42da9c78c33a4dbbec05db224339895b9ca4
ar.chusovitin <ar.chusovitin@tensor.ru>
2021-01-11 16:03:17 +0700

    https://trello.com/c/Rrs2cqHV

--- polygon_platform/app/app/tasks/models.py
+++ polygon_platform/app/app/tasks/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from polygon_platform.app.app.files.models import Files
 
 
 class Task(models.Model):
     """
     Модель задания
     """
-    pass
+    name = models.CharField(max_length=256)
+    flag = models.CharField(max_length=256)
+    picture = models.ImageField(upload_to='uploads/picture/')
+    description = models.TextField()
+    files = models.ForeignKey(Files, on_delete=models.CASCADE)
